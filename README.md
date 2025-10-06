dbe5e72eb3b9e50e39e847873ad33b7f133bce14
GermanBluefox <dogafox@gmail.com>
2025-10-06 19:51:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# @iobroker/modbus
+This is a library that allows you to implement ioBroker Adapter that communicates via modbus with some device.
+
+It could accept TSV file as a configuration.
+
+## Test
+There are some programs in folder `test` to test the TCP communication:
+- Ananas32/64 is a slave simulator (only holding registers and inputs, no coils and digital inputs)
+- RMMS is master simulator
+- mod_RSsim.exe is a slave simulator. It can be that you need [Microsoft Visual C++ 2008 SP1 Redistributable Package](https://www.microsoft.com/en-us/download/details.aspx?id=5582) to start it (because of Side-By-Side error).
+
+<!--
+	### **WORK IN PROGRESS**
+-->
+## Changelog
+### 0.0.1
+* (bluefox) initial commit
+
+## License
+The MIT License (MIT)
+
+Copyright (c) 2015-2025 Bluefox <dogafox@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ src/convert.ts
@@ -0,0 +1,112 @@
+import type { Register, RegisterType } from './types';
+import { tsv2json } from 'tsv-json';
+import { readFileSync, existsSync } from 'node:fs';
+
+type RegisterField = {
+    name: string;
+    type?: 'checkbox';
+};
+
+const coils: RegisterField[] = [
+    { name: '_address' },
+    { name: 'name' },
+    { name: 'description' },
+    { name: 'formula' },
+    { name: 'role' },
+    { name: 'room' },
+    { name: 'poll', type: 'checkbox' },
+    { name: 'wp', type: 'checkbox' },
+    { name: 'cw', type: 'checkbox' },
+    { name: 'isScale', type: 'checkbox' },
+];
+
+const disInputs: RegisterField[] = [
+    { name: '_address' },
+    { name: 'name' },
+    { name: 'description' },
+    { name: 'formula' },
+    { name: 'role' },
+    { name: 'room' },
+    { name: 'cw', type: 'checkbox' },
+    { name: 'isScale', type: 'checkbox' },
+];
+
+const holdingRegs: RegisterField[] = [
+    { name: '_address' },
+    { name: 'name' },
+    { name: 'description' },
+    { name: 'unit' },
+    { name: 'type' },
+    { name: 'len' },
+    { name: 'factor' },
+    { name: 'offset' },
+    { name: 'formula' },
+    { name: 'role' },
+    { name: 'room' },
+    { name: 'poll', type: 'checkbox' },
+    { name: 'wp', type: 'checkbox' },
+    { name: 'cw', type: 'checkbox' },
+    { name: 'isScale', type: 'checkbox' },
+];
+
+const inputRegs: RegisterField[] = [
+    { name: '_address' },
+    { name: 'name' },
+    { name: 'description' },
+    { name: 'unit' },
+    { name: 'type' },
+    { name: 'len' },
+    { name: 'factor' },
+    { name: 'offset' },
+    { name: 'formula' },
+    { name: 'role' },
+    { name: 'room' },
+    { name: 'cw', type: 'checkbox' },
+    { name: 'isScale', type: 'checkbox' },
+];
+
+export default function tsv2registers(type: RegisterType, fileNameOrText: string): Register[] {
+    if (fileNameOrText.toLowerCase().endsWith('.tsv')) {
+        if (existsSync(fileNameOrText)) {
+            fileNameOrText = readFileSync(fileNameOrText).toString();
+        }
+    }
+    let propsFields: RegisterField[] | undefined;
+
+    if (type === 'coils') {
+        propsFields = coils;
+    } else if (type === 'inputRegs') {
+        propsFields = inputRegs;
+    } else if (type === 'holdingRegs') {
+        propsFields = holdingRegs;
+    } else if (type === 'disInputs') {
+        propsFields = disInputs;
+    }
+
+    if (!propsFields) {
+        throw new Error('Unknown register type');
+    }
+
+    const data: (string | boolean)[][] = tsv2json(
+        fileNameOrText.endsWith('\n') ? fileNameOrText : `${fileNameOrText}\n`,
+    );
+    const fields = data.shift();
+    if (fields) {
+        for (const index in propsFields) {
+            if (propsFields[index].name !== fields[index]) {
+                throw new Error(`Unexpected field ${index} for ${fields[index]}`);
+            }
+        }
+    }
+
+    return data.map(itemValues => {
+        const item: Register = {} as Register;
+        for (const index in propsFields) {
+            if (propsFields[index].type === 'checkbox') {
+                itemValues[index] = itemValues[index] === 'true';
+            }
+            (item as unknown as Record<string, string | boolean | number>)[propsFields[index].name] = itemValues[index];
+        }
+        return item;
+    });
+}
